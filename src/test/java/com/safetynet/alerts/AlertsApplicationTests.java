73c8aeba8ec340f979fce6b9a8fc890aa429f865
pochi <pochi>
2024-02-12 09:17:54 +0100

    Fixing errors

--- src/test/java/com/safetynet/alerts/AlertsApplicationTests.java
+++ src/test/java/com/safetynet/alerts/AlertsApplicationTests.java
@@ -1,36 +1,49 @@
 package com.safetynet.alerts;
-import com.safetynet.alerts.controller.TestController;
 import com.safetynet.alerts.model.JsonElts;
 import com.safetynet.alerts.model.JsonReader;
 import com.safetynet.alerts.controller.CrudController;
+import com.safetynet.alerts.model.Person;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import static org.mockito.Mockito.when;
-import java.io.IOException;
 
-@WebMvcTest(controllers = TestController.class)
-@Import(TestController.class)
+import java.io.IOException;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = CrudController.class)
+@Import(CrudController.class)
 public class AlertsApplicationTests {
 
 	@Autowired
 	@MockBean
 	private JsonReader jsonReader;
 
+	private JsonElts jsonElts;
+
 	@Autowired
 	private MockMvc mockMvc;
+
+	@InjectMocks
+	CrudController crudController;
+
+
+	public JsonElts sharedJson() throws IOException {
+		return jsonReader.getJson();
+	}
 
 	@Test
 	public void testYourEndpoint() throws Exception {
 		JsonElts jsonElts = new JsonElts();
 		when(jsonReader.getJson()).thenReturn(jsonElts);
-		mockMvc.perform(MockMvcRequestBuilders.get("/test/person"))
+		mockMvc.perform(MockMvcRequestBuilders.delete("/person/johnboyd"))
 				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 }
